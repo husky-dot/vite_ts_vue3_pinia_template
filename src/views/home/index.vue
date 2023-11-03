78b1cd192c94c4a1d669afd80728778168b94824
wangzhiyi <449245884@qq.com>
2023-11-03 16:45:10 +0800

    feat: 完成模板

---
+++ src/views/home/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div class=" text-[100px] text-red-50">home</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
